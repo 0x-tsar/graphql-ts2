e3e1720c59d8e910da61f8f83da069d9720e0bf4
Gustavo <contato.gustavofernandes@hotmail.com>
2022-04-05 21:46:14 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+const { ApolloServer, gql } = require("apollo-server");
+const typeDefs = {};
+const resolvers = {};
+const server = new ApolloServer({ typeDefs, resolvers });
+server.listen().then(({ url }) => console.log(`server started at: ${url}`));
